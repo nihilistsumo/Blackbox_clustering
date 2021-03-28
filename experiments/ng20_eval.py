e1648393487aaea22f32772b39d6a6c8a1e99fc6
Sumanta  Kashyapi <sk1105@wildcats.unh.edu>
2021-03-28 14:43:08 -0400

    ng20 eval model

--- experiments/ng20_eval.py
+++ experiments/ng20_eval.py
@@ -1,5 +1,6 @@
 import argparse
 import pickle
+import torch
 from sentence_transformers import SentenceTransformer
 from util.Evaluator import ClusterEvaluator
 
@@ -10,10 +11,18 @@ args = parser.parse_args()
 test_data_path = args.test_data
 model_paths = args.model_paths
 
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+    print('CUDA is available and using device: ' + str(device))
+else:
+    device = torch.device('cpu')
+    print('CUDA not available, using device: ' + str(device))
+
 with open(test_data_path, 'rb') as f:
     test_cluster_data = pickle.load(f)
 test_evaluator = ClusterEvaluator.from_input_examples(test_cluster_data)
 for mp in model_paths:
     m = SentenceTransformer(mp)
+    m.to(device)
     print('Model: '+mp.split('/')[len(mp.split('/'))-1])
     m.evaluate(test_evaluator)
