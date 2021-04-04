09f86d2a4ca8685eaea19a20642f4a5e9c3e8c0f
Sumanta  Kashyapi <sk1105@wildcats.unh.edu>
2021-04-03 20:55:16 -0400

    add relu reg train

--- experiments/train_model.py
+++ experiments/train_model.py
@@ -20,7 +20,8 @@ import random
 from collections import Counter
 from util.Data import InputTRECCARExample
 from util.Evaluator import ClusterEvaluator
-from model.BBCluster import BBClusterLossModel, BBSpectralClusterLossModel, CustomSentenceTransformer, DBCLossModel, BinaryLoss
+from model.BBCluster import BBClusterLossModel, BBSpectralClusterLossModel, CustomSentenceTransformer, DBCLossModel, \
+    BinaryLoss, BBClusterLossReluRegModel
 import argparse
 random.seed(42)
 torch.manual_seed(42)
@@ -60,6 +61,58 @@ def run_fixed_lambda_bbcluster(train_cluster_data, val_cluster_data, test_cluste
                                                 reg_const=config_dict.get('reg', reg), beta=beta)
     else:
         loss_model = BBClusterLossModel(model=model, device=device,
+                                        lambda_val=config_dict.get('lambda_val', lambda_val),
+                                        reg_const=config_dict.get('reg', reg))
+    # reg_loss_model = ClusterDistLossModel(model=model)
+
+    train_dataloader = DataLoader(train_cluster_data, shuffle=True, batch_size=train_batch_size)
+    # train_dataloader2 = DataLoader(train_cluster_data, shuffle=True, batch_size=train_batch_size)
+    evaluator = ClusterEvaluator.from_input_examples(val_cluster_data, use_model_device)
+    test_evaluator = ClusterEvaluator.from_input_examples(test_cluster_data, use_model_device)
+
+    warmup_steps = int(len(train_dataloader) * num_epochs * warmup_frac)  # 10% of train data
+
+    print("Raw BERT embedding performance")
+    model.to(device)
+    evaluator(model, output_path)
+
+    # Train the model
+    model.fit(train_objectives=[(train_dataloader, loss_model)],
+              evaluator=evaluator,
+              test_evaluator=test_evaluator,
+              epochs=num_epochs,
+              evaluation_steps=eval_steps,
+              warmup_steps=warmup_steps,
+              output_path=output_path,
+              logger=task.get_logger())
+
+def run_fixed_lambda_relu_reg_bbcluster(train_cluster_data, val_cluster_data, test_cluster_data, output_path, train_batch_size, eval_steps,
+                               num_epochs, warmup_frac, lambda_val, reg, beta, loss_name, use_model_device, model_name='distilbert-base-uncased', out_features=256):
+    task = Task.init(project_name='BB Clustering', task_name='bbclustering_fixed_lambda')
+    config_dict = {'lambda_val': lambda_val, 'reg': reg}
+    config_dict = task.connect(config_dict)
+    if torch.cuda.is_available():
+        device = torch.device('cuda')
+        print('CUDA is available and using device: '+str(device))
+    else:
+        device = torch.device('cpu')
+        print('CUDA not available, using device: '+str(device))
+    ### Configure sentence transformers for training and train on the provided dataset
+    # Use Huggingface/transformers model (like BERT, RoBERTa, XLNet, XLM-R) for mapping tokens to embeddings
+    word_embedding_model = models.Transformer(model_name)
+
+    # Apply mean pooling to get one fixed sized sentence vector
+    pooling_model = models.Pooling(word_embedding_model.get_word_embedding_dimension(),
+                                   pooling_mode_mean_tokens=True,
+                                   pooling_mode_cls_token=False,
+                                   pooling_mode_max_tokens=False)
+
+    doc_dense_model = models.Dense(in_features=pooling_model.get_sentence_embedding_dimension(), out_features=out_features,
+                                   activation_function=nn.Tanh())
+
+    model = CustomSentenceTransformer(modules=[word_embedding_model, pooling_model, doc_dense_model])
+    # model = SentenceTransformer(modules=[word_embedding_model, pooling_model])
+    loss_model = BBClusterLossReluRegModel(model=model, device=device,
                                         lambda_val=config_dict.get('lambda_val', lambda_val),
                                         reg_const=config_dict.get('reg', reg))
     # reg_loss_model = ClusterDistLossModel(model=model)
