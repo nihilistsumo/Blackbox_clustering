b54a38aa3a048cd1d1b178a6d082b3971fba2839
Sumanta  Kashyapi <sk1105@wildcats.unh.edu>
2021-04-24 01:38:29 -0400

    add cluster viz full

--- temp/cluster_vis.py
+++ temp/cluster_vis.py
@@ -27,3 +27,20 @@ def viz_cluster(page, emb_dict_path, s=5):
     ax = sns.scatterplot(x=pca_embeddings[:, 0], y=pca_embeddings[:, 1], hue=labels, palette='deep', s=s)
     plt.title(page)
     plt.show()
+
+def compare_viz_cluster(pages, emb_dict_path1, emb_dict_path2, save_dir_path, s=5, xsize=50, ysize=25):
+    emb_dict1 = np.load(emb_dict_path1, allow_pickle=True)
+    emb_dict2 = np.load(emb_dict_path2, allow_pickle=True)
+    for i in range(len(pages)):
+        print(pages[i])
+        vecs1 = emb_dict1[()][pages[i]]['vec']
+        labels1 = emb_dict1[()][pages[i]]['label']
+        pca1 = pca(vecs1, 2)[0]
+        vecs2 = emb_dict2[()][pages[i]]['vec']
+        labels2 = emb_dict2[()][pages[i]]['label']
+        pca2 = pca(vecs2, 2)[0]
+        fig, axes = plt.subplots(1, 2, figsize=(xsize, ysize))
+        fig.suptitle(pages[i])
+        sns.scatterplot(ax=axes[0], x=pca1[:, 0], y=pca1[:, 1], hue=labels1, palette='deep', s=s)
+        sns.scatterplot(ax=axes[1], x=pca2[:, 0], y=pca2[:, 1], hue=labels2, palette='deep', s=s)
+        plt.savefig(save_dir_path+'/'+pages[i]+'.png')
