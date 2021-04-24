b000d56c9ae1be53c02752db97c5f21f2e5b91d6
Sumanta  Kashyapi <sk1105@wildcats.unh.edu>
2021-04-23 20:55:00 -0400

    add cluster viz np based pca

--- temp/cluster_vis.py
+++ temp/cluster_vis.py
@@ -4,13 +4,25 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
+
+def cov(X):
+    return np.dot(X.T, X) / X.shape[0]
+
+def pca(data, pc_count = None):
+    data -= np.mean(data, 0)
+    data /= np.std(data, 0)
+    C = cov(data)
+    E, V = np.linalg.eigh(C)
+    key = np.argsort(E)[::-1][:pc_count]
+    E, V = E[key], V[:, key]
+    U = np.dot(data, V)  # used to be dot(V.T, data.T).T
+    return U, E, V
+
 def viz_cluster(page, emb_dict_path):
     emb_dict = np.load(emb_dict_path, allow_pickle=True)
     vecs = emb_dict[()][page]['vec']
     labels = emb_dict[()][page]['label']
     print(vecs.shape)
-    pca = PCA(n_components=2)
-    doc_embeddings_np_scaled = StandardScaler().fit_transform(vecs)
-    pca_embeddings = pca.fit_transform(doc_embeddings_np_scaled)
+    pca_embeddings = pca(vecs, 2)[0]
     ax = sns.scatterplot(x=pca_embeddings[:, 0], y=pca_embeddings[:, 1], hue=labels, palette='deep')
     plt.show()
